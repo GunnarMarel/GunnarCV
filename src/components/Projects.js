665af766fc2989eab2919a75bc333fd03996e562
Gunnar Marel <gunnarm8@gmail.com>
2018-09-09 12:51:52 +0000

    Github butttons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,7 @@ class Projects extends Component {
                   <p><strong>Peer to peer car rental a project for HBV</strong></p>
                   <p>Java, Spring Framework, PostgreSQL</p>
                   <a href="https://infinite-mountain-61347.herokuapp.com/welcome" target="_blank" rel="noopener noreferrer"><button className="btn success">Check it out </button></a>
-                  <a href="https://infinite-mountain-61347.herokuapp.com/welcome" target="_blank" rel="noopener noreferrer"><button className="btn success">Check it out </button></a>
+                  <a href="https://github.com/GunnarMarel/Team11Project/tree/master/Team11Project03" target="_blank" rel="noopener noreferrer"><button className="btn success">Github </button></a>
                 </div>
             </div>
             <div className="item">
@@ -29,6 +29,7 @@ class Projects extends Component {
               <p><strong>A Pacman remake with my teacher</strong></p>
               <p>Javascript, HTML, CSS</p>
               <a href="https://karitassif.github.io/Patman/" target="_blank" rel="noopener noreferrer"><button className="btn success">Check it out </button></a>
+              <a href="https://github.com/karitassif/Patman" target="_blank" rel="noopener noreferrer"><button className="btn success">Github </button></a>
           </div>
         </div>
         <div className="item">
@@ -40,6 +41,7 @@ class Projects extends Component {
               <p><strong>A app with Gljúfrasteinn</strong></p>
               <p>React Native, RESTful api, Android, IOS</p>
               <a href="https://expo.io/@mimiqkz/laxness-react" target="_blank" rel="noopener noreferrer"><button className="btn success">Check it out </button></a>
+              <a href="https://github.com/mimiqkz/Laxness" target="_blank" rel="noopener noreferrer"><button className="btn success">Github </button></a>
             </div>
           </div>
         </div>
